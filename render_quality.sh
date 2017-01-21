f5ec83c446f2ddf83b615dc86ad3eda73caa0815
bmenrigh <bmenrigh@braundonenright.net>
2017-01-21 01:19:27 +0000

    Improved render quality and size for render job.

--- render_quality.sh
+++ render_quality.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nice -n 19 povray -D -P +WT1 +Q11 +A0.1 +W800 +H800 +AM1 +R4 -J +Ojob_$1.png $1.pov > /dev/null 2>&1
+nice -n 19 povray -D -P +WT2 +Q11 +A0.2 +W1000 +H1000 +AM1 +R5 -J1 +Ojob_$1.png $1.pov > /dev/null 2>&1
