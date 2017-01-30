c8dafc4db080c40c1cf0884fee744bff15f692ae
bmenrigh <bmenrigh@braundonenright.net>
2017-01-30 01:50:01 +0000

    Made the preview render scripts use 4 threads

--- render_fast.sh
+++ render_fast.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nice -n 19 povray -D -P +WT1 +Q2 -A +W440 +H440 +Ofast_$1.png $1.pov > /dev/null 2>&1
+nice -n 19 povray -D -P +WT4 +Q2 -A +W440 +H440 +Ofast_$1.png $1.pov > /dev/null 2>&1
 
